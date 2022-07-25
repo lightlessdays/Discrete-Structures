fac507be603de1e3ab2dced437dbe736bc49f55c
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-25 17:22:43 +0530

    Create fibonacciseries.cpp

---
+++ fibonacciseries.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+  n-=2;
+  int fm=1;
+  int f=1;
+  int fn=1;
+
+  for(int i=0;i<n;i++){
+    fn=f+fm;
+    fm=f;
+    f=fn;
+  }
+  return fn;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of terms:";
+    cin>>n;
+    cout<<fibonacci(n);
+}

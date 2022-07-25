db8e58b5fa9a2909547d18b2c2f761c7c3bb57f2
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-25 23:58:43 +0530

    Update permutations of a given string.cpp

--- permutations of a given string.cpp
+++ permutations of a given string.cpp
@@ -9,12 +9,33 @@ PERMUTATION WITH DUPLICATES ALLOWED...
 #include <bits/stdc++.h> 
 using namespace std; 
 
-
+void permute(string a,int startIndex,int endIndex){
+     // Base case 
+    if (startIndex == endIndex) 
+        cout<<a<<endl; 
+    else
+    { 
+        // Permutations made 
+        for (int i = startIndex; i <= endIndex; i++) 
+        { 
+  
+            // Swapping done 
+            swap(a[startIndex], a[i]); 
+  
+            // Recursion called 
+            permute(a, startIndex+1, endIndex); 
+  
+            //backtrack . BACKTRACKING SEEKH LENI CHAHIYE THI. CRY AARA H.
+            swap(a[startIndex], a[i]); 
+        } 
+    } 
+}
 
 int main(){
-  string a;
+  string a="";
   cout<<"Enter the string:";
   cin>>a;
+  int n=a.size();
   permute(a,0,n-1);
   return 0;
 }

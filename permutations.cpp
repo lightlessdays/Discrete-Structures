1a014faf6e2453d92110b1547ede12d00b6bb8f2
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-26 01:40:13 +0530

    Create permutations.cpp

---
+++ permutations.cpp
@@ -0,0 +1,20 @@
+/*
+ASAAN SOLUTION. FACTORIAL KA RECUSTION. NCR = N!/R!(N-R)!
+*/
+#include<iostream>
+using namespace std;
+int factorial(int n) {
+ if (n <= 1)
+     return 1;
+ else
+     return(n*(factorial(n-1)));
+}
+
+int main(){
+	int n,r;
+	cout<<"ENTER n: ";
+	cin>>n;
+	cout<<"Enter r: ";
+	cin>>r;
+	cout<<"The value of nPr is: "<<(factorial(n)/(factorial(n-r));
+}

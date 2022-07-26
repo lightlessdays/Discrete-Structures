3ed9938276ee2e0b19d119638b3e2e12898c1bf5
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-26 14:29:34 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1) return n;
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+int main() {
+   int n;
+   cout<<"Enter the nth term: ";
+   cin>>n;
+   cout<<fibonacci(n);
+}
